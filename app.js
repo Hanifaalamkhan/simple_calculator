a4149b79631b60d35636f7d3ebd3db5a1e83f693
Hanifaalamkhan <hanifakhan237@gmail.com>
2024-05-07 02:35:39 +0500

    coding

---
+++ app.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const calculator = async () => {
+    const answer = await inquirer.prompt([
+        { message: "Enter the first number", type: "number", name: "firstNumber" },
+        { message: "Enter the second number", type: "number", name: "secondNumber" },
+        {
+            message: "select the one operator to perform operation",
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        },
+    ]);
+    //conditional statement
+    if (answer.operator === "Addition") {
+        console.log(answer.firstNumber + answer.secondNumber);
+    }
+    else if (answer.operator === "Subtration") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operator === "Multiplication") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operator === "Division") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    else {
+        console.log("please select the valid operator");
+    }
+};
+calculator();
